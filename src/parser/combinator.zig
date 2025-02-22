f89133b43d96e0206599fa7e63804a47159774bb
yamada031016 <ymwk.sota@gmail.com>
2025-02-22 11:14:07 +0900

    add many_till()

--- src/parser/combinator.zig
+++ src/parser/combinator.zig
@@ -69,3 +69,40 @@ test "many1 char" {
     try std.testing.expectEqual(3, result.result.len);
     try std.testing.expectEqualStrings("###", try std.mem.concat(std.heap.page_allocator, u8, result.result));
 }
+
+pub fn many_till(parser: ParserFunc, end: ParserFunc) fn ([]const u8) anyerror!ParseResult([]const []const u8) {
+    return struct {
+        fn parse(input: []const u8) !ParseResult([]const []const u8) {
+            var array = std.ArrayList([]const u8).init(std.heap.page_allocator);
+            var rest_input = input;
+            while (true) {
+                const r = parser(rest_input);
+                if (r) |res| {
+                    array.append(res.result) catch |err| {
+                        return err;
+                    };
+                    if (res.rest.len == 0) {
+                        return ParseResult([]const []const u8){ .rest = res.rest, .result = try array.toOwnedSlice() };
+                    } else {
+                        rest_input = res.rest;
+                    }
+
+                    const facedEnd = end(rest_input);
+                    if (facedEnd) |endResult| {
+                        return ParseResult([]const []const u8){ .rest = endResult.rest, .result = try array.toOwnedSlice() };
+                    } else |e| {
+                        switch (e) {
+                            else => {},
+                        }
+                    }
+                } else |e| {
+                    if (array.getLastOrNull()) |_| {
+                        return ParseResult([]const []const u8){ .rest = rest_input, .result = try array.toOwnedSlice() };
+                    } else {
+                        return e;
+                    }
+                }
+            }
+        }
+    }.parse;
+}
