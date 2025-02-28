5e1b56e779efc26b5a959717e8c3eb756e27e5e7
yamada031016 <ymwk.sota@gmail.com>
2025-02-28 18:10:59 +0900

    wip

---
+++ src/bytes/bytes.zig
@@ -0,0 +1,76 @@
+//! This module provides combinators for bytes strings.
+const std = @import("std");
+const ab = @import("../abelha.zig");
+const ParserFunc = ab.ParserFunc;
+const IResult = ab.IResult;
+const ParseError = ab.ParseError;
+
+/// Return `input[0..N]`, a slice of N bytes from the beginning of the input.
+/// Return `ParseError.InvalidFormat` when the size of input is less than N.
+pub fn take(cnt: usize) ParserFunc {
+    return struct {
+        fn parse(input: []const u8) !IResult {
+            if (input.len < cnt) {
+                return ParseError.InvalidFormat;
+            }
+            return IResult{ .rest = input[cnt..], .result = input[0..cnt] };
+        }
+    }.parse;
+}
+
+/// Recognizes the pattern specified by the `needle` argument
+/// The input is compared to see if it matches the pattern and the matching portion is returned.
+/// If the size of the input is less than the size of the pattern, `ParseError.NeedleTooShort` is returned.
+/// `ParseError.NotFound` is returned when no pattern is found.
+pub fn tag(needle: []const u8) ParserFunc {
+    return struct {
+        fn parse(input: []const u8) !IResult {
+            if (input.len < needle.len) {
+                std.log.debug("\ntarget string: {s}(len: {})\nneedle: {s}(len: {})\n", .{ input, input.len, needle, needle.len });
+                return ParseError.NeedleTooShort;
+            } else {
+                if (std.mem.eql(u8, input[0..needle.len], needle)) {
+                    std.log.debug("successed.\ntarget string: {s}(len: {})\nneedle: {s}(len: {})\n", .{ input, input.len, needle, needle.len });
+                    return IResult{ .rest = input[needle.len..], .result = needle };
+                } else {
+                    std.log.debug("\ntarget string: {s}\nneedle: {s}\n", .{ input, needle });
+                    return ParseError.NotFound;
+                }
+            }
+        }
+    }.parse;
+}
+
+/// Returns a sequence of bytes as slices until the specified pattern is found.
+pub fn take_until(end: []const u8) ParserFunc {
+    return struct {
+        fn parse(input: []const u8) !IResult {
+            const index = std.mem.indexOf(u8, input, end);
+            if (index) |idx| {
+                return IResult{ .rest = input[idx..], .result = input[0..idx] };
+            } else {
+                return ParseError.NotFound;
+            }
+        }
+    }.parse;
+}
+
+/// Returns a sequence of bytes as a slice until the specified character is found
+pub fn is_not(needle: []const u8) ParserFunc {
+    return struct {
+        fn parse(input: []const u8) !IResult {
+            if (input.len == 0) {
+                return ParseError.InputTooShort;
+            }
+
+            for (input, 0..) |c, i| {
+                for (needle) |n| {
+                    if (c == n) {
+                        return IResult{ .rest = input[i..], .result = input[0..i] };
+                    }
+                }
+            }
+            return IResult{ .rest = "", .result = input };
+        }
+    }.parse;
+}
---
+++ src/combinator/combinator.zig
@@ -0,0 +1,58 @@
+//! This module provides general purpose combinators.
+const std = @import("std");
+const ab = @import("../abelha.zig");
+const ParserFunc = ab.ParserFunc;
+const IResult = ab.IResult;
+const ParseError = ab.ParseError;
+const ParseResult = ab.ParseResult;
+
+/// Recognizes that the input has reached the end of the line.
+pub fn eof(input: []const u8) !IResult {
+    if (input.len == 0) {
+        return IResult{ .rest = "", .result = "" };
+    } else {
+        return ParseError.NotFound;
+    }
+}
+
+/// Reads ahead without consuming input whether the specified parser will succeed or not.
+pub fn peek(parser: ParserFunc) ParserFunc {
+    return struct {
+        fn parse(input: []const u8) !IResult {
+            const result = try parser(input);
+            return IResult{ .rest = input, .result = result.result };
+        }
+    }.parse;
+}
+
+/// Succeeds if the child parser returns an error.
+pub fn not(expect_fail_parser: ParserFunc) ParserFunc {
+    return struct {
+        fn parse(input: []const u8) !IResult {
+            const result = expect_fail_parser(input);
+            if (result) |_| {
+                return ParseError.NotFound;
+            } else |e| {
+                switch (e) {
+                    else => return IResult{ .rest = input, .result = "" },
+                }
+            }
+        }
+    }.parse;
+}
+
+/// Optional parser, returns the result of a successful parse or the input if it fails.
+pub fn opt(opt_parser: ParserFunc) ParserFunc {
+    return struct {
+        fn parse(input: []const u8) !IResult {
+            const result = opt_parser(input);
+            if (result) |res| {
+                return IResult{ .rest = res.rest, .result = res.result };
+            } else |e| {
+                switch (e) {
+                    else => return IResult{ .rest = input, .result = "" },
+                }
+            }
+        }
+    }.parse;
+}
