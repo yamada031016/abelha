451394fbb336126871c6c36859409f66ee73edd6
yamada031016 <ymwk.sota@gmail.com>
2025-03-06 12:22:40 +0900

    small fix

--- src/abelha.zig
+++ src/abelha.zig
@@ -82,201 +82,205 @@ fn examineArgType(arg: anytype) ParserArgType {
 // args[2]: parse function input
 // args[3..]: optional infomation
 pub fn report(err: anyerror, args: anytype) void {
-    if (@import("builtin").mode == .Debug or @import("builtin").mode == .ReleaseSafe) {
-        const argType = examineArgType(args[1]);
-        switch (err) {
-            error.InputTooShort => {
-                switch (argType) {
-                    ParserArgType.String => |str| std.debug.print(
-                        \\{s}({s}) {s}
-                        \\found: {s}
-                        \\       ^ -- Expected at least {}, but found only {}.
-                    , .{ args[0], str, @errorName(err), args[2], str.len, args[2].len }),
-                    ParserArgType.Tuple => {
-                        std.debug.print(
-                            \\{s}({any}) {s}
-                            \\found: {s}
-                            \\       ^ -- Expected at least {}, but found only {}.
-                        , .{ args[0], args[1], @errorName(err), args[2], args[1].len, args[2].len });
-                    },
-                    ParserArgType.Unit => std.debug.print(
-                        \\
-                        \\{s}() {s}
-                        \\found: {s}
-                        \\       ^
-                    , .{ args[0], @errorName(err), args[2] }),
-                    ParserArgType.ParserFunc => {
-                        std.debug.print(
-                            \\
-                            \\[error] {s}({s}) {s}
-                            \\found: {s}
-                            \\       ^
-                        , .{ args[0], @typeName(@TypeOf(args[1])), @errorName(err), args[2] });
-                    },
-                    else => {
-                        std.debug.print(
-                            \\{s}({any}) {s}
-                            \\found: {s}
-                            \\       ^
-                        , .{ args[0], args[1], @errorName(err), args[2] });
-                    },
-                }
-            },
-            error.NeedleTooLong => {
-                switch (argType) {
-                    ParserArgType.String => |str| {
-                        std.debug.print(
-                            \\
-                            \\[error] {s}("{s}") {s}
-                            \\found: {s}
-                            \\       ^ -- Expected at most {}, but needle requires {}.
-                        , .{ args[0], str, @errorName(err), args[2], args[2].len, str.len });
-                    },
-                    ParserArgType.Tuple => std.debug.print(
-                        \\
-                        \\[error] {s}({any}) {s}
-                        \\┌─
-                        \\│ {s}
-                        \\  ^
-                    , .{ args[0], args[1], @errorName(err), args[2] }),
-                    ParserArgType.Unit => std.debug.print(
-                        \\
-                        \\[error] {s} {s}
-                        \\┌─
-                        \\│ {s}
-                        \\  ^
-                    , .{ args[0], @errorName(err), args[2] }),
-                    ParserArgType.ParserFunc => std.debug.print(
-                        \\
-                        \\[error] {s}({s}) {s}
-                        \\┌─
-                        \\│ {s}
-                        \\  ^
-                    , .{ args[0], @typeName(@TypeOf(args[1])), @errorName(err), args[2] }),
-                    else => std.debug.print(
-                        \\
-                        \\[error] {s}({any}) {s}
-                        \\┌─
-                        \\│ {s}
-                        \\  ^
-                    , .{ args[0], args[1], @errorName(err), args[2] }),
-                }
-            },
-            error.NotFound => {
-                switch (argType) {
-                    ParserArgType.String => {
-                        std.debug.print(
-                            \\
-                            \\[error] {s}("{s}") {s}
-                            \\┌─
-                            \\│ {s}
-                            \\  ^ -- Expected "{s}", but found "{s}".
-                        , .{ args[0], args[1], @errorName(err), args[2], args[1], args[2] });
-                    },
-                    ParserArgType.Tuple => std.debug.print(
-                        \\
-                        \\[error] {s}({any}) {s}
-                        \\┌─
-                        \\│ {s}
-                        \\  ^
-                    , .{ args[0], args[1], @errorName(err), args[2] }),
-                    ParserArgType.Unit => std.debug.print(
-                        \\
-                        \\[error] {s} {s}
-                        \\┌─
-                        \\│ {s}
-                        \\  ^
-                    , .{ args[0], @errorName(err), args[2] }),
-                    ParserArgType.ParserFunc => std.debug.print(
-                        \\
-                        \\[error] {s}({s}) {s}
-                        \\┌─
-                        \\│ {s}
-                        \\  ^
-                    , .{ args[0], @typeName(@TypeOf(args[1])), @errorName(err), args[2] }),
-                    else => std.debug.print(
-                        \\
-                        \\[error] {s}({any}) {s}
-                        \\┌─
-                        \\│ {s}
-                        \\  ^
-                    , .{ args[0], args[1], @errorName(err), args[2] }),
-                }
-            },
-            error.InvalidFormat, error.EmptyMatched => {
-                switch (argType) {
-                    .String => std.debug.print(
-                        \\
-                        \\[error] {s}("{s}") {s}
-                        \\┌─
-                        \\│ {s}
-                        \\  ^ -- Expected "{s}", but found "{s}".
-                    , .{ args[0], args[1], @errorName(err), args[2], args[1], args[2] }),
-                    .Tuple => std.debug.print(
-                        \\
-                        \\[error] {s}({any}) {s}
-                        \\┌─
-                        \\│ {s}
-                        \\  ^
-                    , .{ args[0], args[1], @errorName(err), args[2] }),
-                    .Unit => std.debug.print(
-                        \\
-                        \\[error] {s} {s}
-                        \\┌─
-                        \\│ {s}
-                        \\  ^
-                    , .{ args[0], @errorName(err), args[2] }),
-                    .ParserFunc => std.debug.print(
-                        \\
-                        \\[error] {s}({s}) {s}
-                        \\┌─
-                        \\│ {s}
-                        \\  ^
-                    , .{ args[0], @typeName(@TypeOf(args[1])), @errorName(err), args[2] }),
-                    else => {
-                        std.debug.print(
-                            \\
-                            \\[error] {s}({any}) {s}
-                            \\┌─
-                            \\│ {s}
-                            \\  ^
-                        , .{ args[0], args[1], @errorName(err), args[2] });
-                    },
-                }
-            },
-            error.IntegerOverflow => {
-                switch (argType) {
-                    .Type => |t| std.debug.print(
-                        \\
-                        \\[error] {s}("{s}") {s}
-                        \\┌─
-                        \\│ {s}
-                        \\  ^ -- Integer too {s}.
-                    , .{
-                        args[0],
-                        @typeName(t),
-                        @errorName(err),
-                        args[2],
-                        if (args[2][0] == '-') "small" else "large",
-                    }),
-                    else => unreachable,
-                }
-            },
-            else => {
-                unreachable;
-                // switch (args.len) {
-                //     0 => std.debug.print(
-                //         \\ parse failed.
-                //     , .{}),
-                //     3 => std.debug.print(
-                //         \\{s}({s}) {s}
-                //         \\found: {s}
-                //     , .{ args[0], args[1], @errorName(err), args[2] }),
-                //     else => std.debug.print("much panic arguement: {any}", .{args}),
-                // }
-            },
-        }
+    switch (err) {
+        else => {},
     }
+    _ = args;
+    // if (@import("builtin").mode == .Debug or @import("builtin").mode == .ReleaseSafe) {
+    //     const argType = examineArgType(args[1]);
+    //     switch (err) {
+    //         error.InputTooShort => {
+    //             switch (argType) {
+    //                 ParserArgType.String => |str| std.debug.print(
+    //                     \\{s}({s}) {s}
+    //                     \\found: {s}
+    //                     \\       ^ -- Expected at least {}, but found only {}.
+    //                 , .{ args[0], str, @errorName(err), args[2], str.len, args[2].len }),
+    //                 ParserArgType.Tuple => {
+    //                     std.debug.print(
+    //                         \\{s}({any}) {s}
+    //                         \\found: {s}
+    //                         \\       ^ -- Expected at least {}, but found only {}.
+    //                     , .{ args[0], args[1], @errorName(err), args[2], args[1].len, args[2].len });
+    //                 },
+    //                 ParserArgType.Unit => std.debug.print(
+    //                     \\
+    //                     \\{s}() {s}
+    //                     \\found: {s}
+    //                     \\       ^
+    //                 , .{ args[0], @errorName(err), args[2] }),
+    //                 ParserArgType.ParserFunc => {
+    //                     std.debug.print(
+    //                         \\
+    //                         \\[error] {s}({s}) {s}
+    //                         \\found: {s}
+    //                         \\       ^
+    //                     , .{ args[0], @typeName(@TypeOf(args[1])), @errorName(err), args[2] });
+    //                 },
+    //                 else => {
+    //                     std.debug.print(
+    //                         \\{s}({any}) {s}
+    //                         \\found: {s}
+    //                         \\       ^
+    //                     , .{ args[0], args[1], @errorName(err), args[2] });
+    //                 },
+    //             }
+    //         },
+    //         error.NeedleTooLong => {
+    //             switch (argType) {
+    //                 ParserArgType.String => |str| {
+    //                     std.debug.print(
+    //                         \\
+    //                         \\[error] {s}("{s}") {s}
+    //                         \\found: {s}
+    //                         \\       ^ -- Expected at most {}, but needle requires {}.
+    //                     , .{ args[0], str, @errorName(err), args[2], args[2].len, str.len });
+    //                 },
+    //                 ParserArgType.Tuple => std.debug.print(
+    //                     \\
+    //                     \\[error] {s}({any}) {s}
+    //                     \\┌─
+    //                     \\│ {s}
+    //                     \\  ^
+    //                 , .{ args[0], args[1], @errorName(err), args[2] }),
+    //                 ParserArgType.Unit => std.debug.print(
+    //                     \\
+    //                     \\[error] {s} {s}
+    //                     \\┌─
+    //                     \\│ {s}
+    //                     \\  ^
+    //                 , .{ args[0], @errorName(err), args[2] }),
+    //                 ParserArgType.ParserFunc => std.debug.print(
+    //                     \\
+    //                     \\[error] {s}({s}) {s}
+    //                     \\┌─
+    //                     \\│ {s}
+    //                     \\  ^
+    //                 , .{ args[0], @typeName(@TypeOf(args[1])), @errorName(err), args[2] }),
+    //                 else => std.debug.print(
+    //                     \\
+    //                     \\[error] {s}({any}) {s}
+    //                     \\┌─
+    //                     \\│ {s}
+    //                     \\  ^
+    //                 , .{ args[0], args[1], @errorName(err), args[2] }),
+    //             }
+    //         },
+    //         error.NotFound => {
+    //             switch (argType) {
+    //                 ParserArgType.String => {
+    //                     std.debug.print(
+    //                         \\
+    //                         \\[error] {s}("{s}") {s}
+    //                         \\┌─
+    //                         \\│ {s}
+    //                         \\  ^ -- Expected "{s}", but found "{s}".
+    //                     , .{ args[0], args[1], @errorName(err), args[2], args[1], args[2] });
+    //                 },
+    //                 ParserArgType.Tuple => std.debug.print(
+    //                     \\
+    //                     \\[error] {s}({any}) {s}
+    //                     \\┌─
+    //                     \\│ {s}
+    //                     \\  ^
+    //                 , .{ args[0], args[1], @errorName(err), args[2] }),
+    //                 ParserArgType.Unit => std.debug.print(
+    //                     \\
+    //                     \\[error] {s} {s}
+    //                     \\┌─
+    //                     \\│ {s}
+    //                     \\  ^
+    //                 , .{ args[0], @errorName(err), args[2] }),
+    //                 ParserArgType.ParserFunc => std.debug.print(
+    //                     \\
+    //                     \\[error] {s}({s}) {s}
+    //                     \\┌─
+    //                     \\│ {s}
+    //                     \\  ^
+    //                 , .{ args[0], @typeName(@TypeOf(args[1])), @errorName(err), args[2] }),
+    //                 else => std.debug.print(
+    //                     \\
+    //                     \\[error] {s}({any}) {s}
+    //                     \\┌─
+    //                     \\│ {s}
+    //                     \\  ^
+    //                 , .{ args[0], args[1], @errorName(err), args[2] }),
+    //             }
+    //         },
+    //         error.InvalidFormat, error.EmptyMatched => {
+    //             switch (argType) {
+    //                 .String => std.debug.print(
+    //                     \\
+    //                     \\[error] {s}("{s}") {s}
+    //                     \\┌─
+    //                     \\│ {s}
+    //                     \\  ^ -- Expected "{s}", but found "{s}".
+    //                 , .{ args[0], args[1], @errorName(err), args[2], args[1], args[2] }),
+    //                 .Tuple => std.debug.print(
+    //                     \\
+    //                     \\[error] {s}({any}) {s}
+    //                     \\┌─
+    //                     \\│ {s}
+    //                     \\  ^
+    //                 , .{ args[0], args[1], @errorName(err), args[2] }),
+    //                 .Unit => std.debug.print(
+    //                     \\
+    //                     \\[error] {s} {s}
+    //                     \\┌─
+    //                     \\│ {s}
+    //                     \\  ^
+    //                 , .{ args[0], @errorName(err), args[2] }),
+    //                 .ParserFunc => std.debug.print(
+    //                     \\
+    //                     \\[error] {s}({s}) {s}
+    //                     \\┌─
+    //                     \\│ {s}
+    //                     \\  ^
+    //                 , .{ args[0], @typeName(@TypeOf(args[1])), @errorName(err), args[2] }),
+    //                 else => {
+    //                     std.debug.print(
+    //                         \\
+    //                         \\[error] {s}({any}) {s}
+    //                         \\┌─
+    //                         \\│ {s}
+    //                         \\  ^
+    //                     , .{ args[0], args[1], @errorName(err), args[2] });
+    //                 },
+    //             }
+    //         },
+    //         error.IntegerOverflow => {
+    //             switch (argType) {
+    //                 .Type => |t| std.debug.print(
+    //                     \\
+    //                     \\[error] {s}("{s}") {s}
+    //                     \\┌─
+    //                     \\│ {s}
+    //                     \\  ^ -- Integer too {s}.
+    //                 , .{
+    //                     args[0],
+    //                     @typeName(t),
+    //                     @errorName(err),
+    //                     args[2],
+    //                     if (args[2][0] == '-') "small" else "large",
+    //                 }),
+    //                 else => unreachable,
+    //             }
+    //         },
+    //         else => {
+    //             unreachable;
+    //             // switch (args.len) {
+    //             //     0 => std.debug.print(
+    //             //         \\ parse failed.
+    //             //     , .{}),
+    //             //     3 => std.debug.print(
+    //             //         \\{s}({s}) {s}
+    //             //         \\found: {s}
+    //             //     , .{ args[0], args[1], @errorName(err), args[2] }),
+    //             //     else => std.debug.print("much panic arguement: {any}", .{args}),
+    //             // }
+    //         },
+    //     }
+    // }
 }
 
 /// Basic Result type of parser
